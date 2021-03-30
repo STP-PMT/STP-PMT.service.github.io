a698ab59ad547f68c829b8a19c4d9e607cd8f5ec
STP-PMT <62597051+STP-PMT@users.noreply.github.com>
2021-03-31 02:33:24 +0700

    update code

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
-<?php
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+    <?php
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
@@ -15,3 +27,7 @@ require __DIR__ . '/api/bill.php';
 
 
 $app->run();
+?>
+    </script>
+</body>
+</html>
